165d10c605ff416bdf6771043efc5ee4d1dc4612
s-ao513 <130631690+s-ao213@users.noreply.github.com>
2025-05-16 19:56:12 +0900

    合計がマイナスかどうかを判定する

--- test.py
+++ test.py
@@ -6,6 +6,11 @@ number_list = [int(x) for x in numbers.split()]
 
 # 合計を計算
 total = sum(number_list)
+# 答えがマイナスかどうかを判定する
+if total < 0:
+    print("合計はマイナスです。")
+else:
+    print("合計はマイナスではありません。")
 
 # 結果を出力
 print(f"入力された数列の合計: {total}")
